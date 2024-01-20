a0d1574827daf3ee8bb2e63ca2dd41553513b5db
Siddhant Bahuguna <114866610+Siddhantawsum@users.noreply.github.com>
2024-01-20 22:36:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//create a function(you can decide) that logs out the number 42 to the console
+// Call/invoke the function
+
+// function logs()
+// {
+//     console.log(42)
+// } 
+
+// logs()
+
+
+// let lap1=34
+// let lap2=33
+// let lap3=36
+
+// create a function that logs out the sum of all the laps times 
+
+// function logLapTime(){
+//     // let totalTime=lap1+lap2+lap3
+//     // console.log(totalTime)
+
+//     console.log(lap1+lap2+lap3)
+// }
+
+// logLapTime()
+
+
